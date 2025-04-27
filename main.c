a3e72e3831120b99814da05f92d9bec510d3fbc9
Imad-eddine-Mahraz <imad-eddine.mahraz@efrei.net>
2025-04-27 17:42:10 +0200

    ajout du programme principal dans le main

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "bmp8.h"
 
 int main() {
-    printf("Hello, World!\n");
+
+    t_bmp8 *img = bmp8_loadImage("C:\\Users\\jules\\CLionProjects\\image_processing_c\\barbara_gray.bmp");
+    if (img != NULL) {
+        bmp8_printInfo(img);
+        bmp8_negative(img);
+        int pixel_index = 0;
+        unsigned char original_pixel = img->data[pixel_index];
+        unsigned char inverted_pixel = 255 - original_pixel;
+        printf("Pixel %d avant : %d, apres : %d\n", pixel_index, original_pixel, inverted_pixel);
+        bmp8_free(img);
+    } else {
+        printf("Échec du chargement de l'image.\n");
+    }
+
     return 0;
 }
